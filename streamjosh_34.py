1b91bca1cd3d3c094216bf21e56fb0f86edc3ea4
Deo Warouw <125939449+anrygratiodeowarouw@users.noreply.github.com>
2023-09-09 13:20:45 +0800

    Update streamjosh_34.py

--- streamjosh_34.py
+++ streamjosh_34.py
@@ -21,7 +21,7 @@ st.text('The soil data used is the N-SPT data and the soil type is based on the 
 col1, col2,col3 = st.columns(3)
 
 with col1:
-    diameter = st.selectbox('diameter', options=[0.8, 1.0, 1.2], index=0)
+    diameter = st.selectbox('diameter', options=[0.45, 0.5, 0.6, 0.8, 1.0, 1.2], index=0)
 with col2:
     COL = st.number_input('COL', value=0.00)
 with col3:
