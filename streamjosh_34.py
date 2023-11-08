a0a2c3eeb905c5ea639786b9a3231a035fdc5de3
Deo Warouw <125939449+anrygratiodeowarouw@users.noreply.github.com>
2023-11-09 05:45:14 +0700

    Update streamjosh_34.py

--- streamjosh_34.py
+++ streamjosh_34.py
@@ -283,8 +283,7 @@ def plot_by_ei(data):
 
     plt.xticks(fontsize=16)
     plt.yticks(fontsize=16)
-    max_ei = np.max(ei)
-    plt.xlim(0, max_ei)
+    plt.xlim(0)
     plt.ylim(0, max(ei+1))
 
     plt.gca().invert_yaxis()
